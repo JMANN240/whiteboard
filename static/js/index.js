7c5f538d0c313b96113e63cc407220abdf828022
JT Raber <JMANN240raber@gmail.com>
2021-07-20 00:48:13 -0400

    no reloading previews

--- static/js/index.js
+++ static/js/index.js
@@ -48,36 +48,43 @@ if (logged_in) {
         resize_timeout = setTimeout(show_previews, 200)
     });
 
-    var show_previews = () => {
+    var strokes = [];
+
+    var get_strokes = () => {
         $.ajax({
             type: 'GET',
             url: '/api/whiteboard',
             success: (res) => {
-                $('#saved-whiteboards').empty();
-                for (var [whiteboard_id, nickname] of res) {
-                    $('#saved-whiteboards').append(`
-                        <div class='preview-whiteboard' id='preview-${whiteboard_id}'>
-                            <button class='${nickname ? 'nickname' : 'whiteboard_id'}'>${nickname ?? whiteboard_id}</button>
-                            <canvas id='canvas-${whiteboard_id}' width='${window.innerWidth*0.1}' height='${window.innerHeight*0.1}'></canvas>
-                        </div>
-                    `)
-                    $.ajax({
-                        type: 'GET',
-                        url: '/api/strokes',
-                        data: {
-                            whiteboard_id: whiteboard_id
-                        },
-                        success: (res) => {
-                            var ctx = document.querySelector(`#canvas-${res.whiteboard_id}`).getContext('2d')
-                            drawStrokes(ctx, res.strokes, [0,0], 0.1)
-                        }
-                    })
-                }
+                strokes = res;
+                show_previews();
             }
-        })
+        });
     }
 
-    show_previews();
+    var show_previews = () => {
+        $('#saved-whiteboards').empty();
+        for (var [whiteboard_id, nickname] of res) {
+            $('#saved-whiteboards').append(`
+                <div class='preview-whiteboard' id='preview-${whiteboard_id}'>
+                    <button class='${nickname ? 'nickname' : 'whiteboard_id'}'>${nickname ?? whiteboard_id}</button>
+                    <canvas id='canvas-${whiteboard_id}' width='${window.innerWidth*0.1}' height='${window.innerHeight*0.1}'></canvas>
+                </div>
+            `)
+            $.ajax({
+                type: 'GET',
+                url: '/api/strokes',
+                data: {
+                    whiteboard_id: whiteboard_id
+                },
+                success: (res) => {
+                    var ctx = document.querySelector(`#canvas-${res.whiteboard_id}`).getContext('2d')
+                    drawStrokes(ctx, res.strokes, [0,0], 0.1)
+                }
+            })
+        }
+    }
+
+    get_strokes();
 }
 
 $(document).on("click", '.nickname', (e) => {
