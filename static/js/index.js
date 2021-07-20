e62765bd203e01aec290d76ec42ca5b53ba084b8
JT Raber <JMANN240raber@gmail.com>
2021-07-20 01:11:14 -0400

    fixed the reloading

--- static/js/index.js
+++ static/js/index.js
@@ -48,43 +48,55 @@ if (logged_in) {
         resize_timeout = setTimeout(show_previews, 200)
     });
 
-    var strokes = [];
+    var whiteboards = [];
+    var strokes = {};
 
-    var get_strokes = () => {
+    var get_whiteboards = () => {
         $.ajax({
             type: 'GET',
             url: '/api/whiteboard',
             success: (res) => {
-                strokes = res;
-                show_previews();
+                whiteboards = res;
+                get_strokes();
             }
         });
     }
 
-    var show_previews = () => {
-        $('#saved-whiteboards').empty();
-        for (var [whiteboard_id, nickname] of res) {
-            $('#saved-whiteboards').append(`
-                <div class='preview-whiteboard' id='preview-${whiteboard_id}'>
-                    <button class='${nickname ? 'nickname' : 'whiteboard_id'}'>${nickname ?? whiteboard_id}</button>
-                    <canvas id='canvas-${whiteboard_id}' width='${window.innerWidth*0.1}' height='${window.innerHeight*0.1}'></canvas>
-                </div>
-            `)
-            $.ajax({
+    var get_strokes = () => {
+        var deferreds = []
+        for (var [whiteboard_id, nickname] of whiteboards) {
+            deferreds.push($.ajax({
                 type: 'GET',
                 url: '/api/strokes',
                 data: {
                     whiteboard_id: whiteboard_id
                 },
                 success: (res) => {
-                    var ctx = document.querySelector(`#canvas-${res.whiteboard_id}`).getContext('2d')
-                    drawStrokes(ctx, res.strokes, [0,0], 0.1)
+                    strokes[res.whiteboard_id] = res;
                 }
-            })
+            }));
+        }
+        $.when(...deferreds).then(function() {
+            show_previews();
+        });
+    }
+
+    var show_previews = () => {
+        $('#saved-whiteboards').empty();
+        for (var [whiteboard_id, nickname] of whiteboards) {
+            $('#saved-whiteboards').append(`
+                <div class='preview-whiteboard' id='preview-${whiteboard_id}'>
+                    <button class='${nickname ? 'nickname' : 'whiteboard_id'}'>${nickname ?? whiteboard_id}</button>
+                    <canvas id='canvas-${whiteboard_id}' width='${window.innerWidth*0.1}' height='${window.innerHeight*0.1}'></canvas>
+                </div>
+            `)
+            
+            var ctx = document.querySelector(`#canvas-${whiteboard_id}`).getContext('2d');
+            drawStrokes(ctx, strokes[whiteboard_id].strokes, [0,0], 0.1);
         }
     }
 
-    get_strokes();
+    get_whiteboards();
 }
 
 $(document).on("click", '.nickname', (e) => {
